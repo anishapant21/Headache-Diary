d9d40a20fb94650427dd0017ae7b0d40f9a88128
eternal-moon <anishapant1@gmail.com>
2021-08-08 20:52:18 +0545

    Solve error V3

--- src/Context/LogContext.js
+++ src/Context/LogContext.js
@@ -1,5 +1,4 @@
-import React, { useState, createContext } from "react";
-import { useContext } from "react/cjs/react.development";
+import React, { useState, createContext, useContext } from "react";
 
 const LogContext = createContext();
 const ToggleContext = createContext();
